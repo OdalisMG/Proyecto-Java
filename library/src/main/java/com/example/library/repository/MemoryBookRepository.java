e2d3a4ae45addbc6f57092a6d7b14943ae443278
OdalisMG <Godalis64gmail.com>
2023-05-08 16:14:22 -0500

    8/5/2023

---
+++ library/src/main/java/com/example/library/repository/MemoryBookRepository.java
@@ -0,0 +1,45 @@
+package com.example.library.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.library.model.Book;
+
+@Repository
+public class MemoryBookRepository implements IBookRepository {
+    private Map<String, Book> booksMap;
+
+    MemoryBookRepository() {
+        booksMap = new HashMap<>(); 
+    }
+    
+    @Override
+    public List<Book> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public Book get(String bookId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public Book save(Book book) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public Book delete(String bookId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    
+}
