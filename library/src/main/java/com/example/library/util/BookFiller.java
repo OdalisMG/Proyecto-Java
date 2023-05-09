0ec4cfcc5f7c3569b830eae21538dbfcd22c6bf7
OdalisMG <Godalis64gmail.com>
2023-05-09 14:10:21 -0500

    9/5/2023

---
+++ library/src/main/java/com/example/library/util/BookFiller.java
@@ -0,0 +1,14 @@
+package com.example.library.util;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class BookFiller {
+    @PostConstruct
+    public void init()
+    {
+        
+    }
+}
